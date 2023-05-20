789870ea826c618d0170122c463790a575b38376
Fer-Jgu <76271548+Fer-Jg@users.noreply.github.com>
2023-05-20 16:04:58 -0600

    updated global utils

--- utils/js/global.js
+++ utils/js/global.js
@@ -1,19 +1,17 @@
 let rWeLive = !(window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
 
-function evaluateLive(val1, val2){return rWeLive ? val1 : val2;}
+// Set links depending on local or live workspace
+const wiki_links = document.querySelectorAll('.wiki');
+const reglas_links = document.querySelectorAll('.reglas');
+const credits_links = document.querySelectorAll('.credits');
+wiki_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/wiki',"../../wiki/index.html");});
+reglas_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/reglas',"../../reglas/index.html");});
+credits_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/colaboradores',"../../colaboradores/index.html");});
 
-function linkDefiner(){
-
-    const wiki_links = document.querySelectorAll('.wiki');
-    const reglas_links = document.querySelectorAll('.reglas');
-    const credits_links = document.querySelectorAll('.credits');
-
-    wiki_links.forEach(element => {element.href = evaluateLive('http://everlasting.lat/wiki',"../../wiki/index.html");});
-    reglas_links.forEach(element => {element.href = evaluateLive('http://everlasting.lat/reglas',"../../reglas/index.html");});
-    credits_links.forEach(element => {element.href = evaluateLive('http://everlasting.lat/colaboradores',"../../colaboradores/index.html");});
-}
-linkDefiner()
+// Functions
+function evaluateLiveValue(val1, val2){return rWeLive ? val1 : val2;}
+function p(str){console.log(str)}
 
 export default {
-    rWeLive
+    rWeLive, p, evaluateLiveValue
 }
