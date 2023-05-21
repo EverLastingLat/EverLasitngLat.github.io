2bc117349231582d88eebef33bffe38bf5296c8e
Fer-Jgu <76271548+Fer-Jg@users.noreply.github.com>
2023-05-20 18:36:26 -0600

    Temp website

--- utils/js/global.js
+++ utils/js/global.js
@@ -4,9 +4,19 @@ let rWeLive = !(window.location.hostname === 'localhost' || window.location.host
 const wiki_links = document.querySelectorAll('.wiki');
 const reglas_links = document.querySelectorAll('.reglas');
 const credits_links = document.querySelectorAll('.credits');
-wiki_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/wiki',"../../wiki/index.html");});
-reglas_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/reglas',"../../reglas/index.html");});
-credits_links.forEach(element => {element.href = evaluateLiveValue('http://everlasting.lat/colaboradores',"../../colaboradores/index.html");});
+wiki_links.forEach(element => {
+  element.href = evaluateLiveValue('http://wiki.everlasting.lat', '../../wiki/index.md');
+  element.target = '_blank';
+});
+reglas_links.forEach(element => {
+  element.href = evaluateLiveValue('http://everlasting.lat/reglas', '../../reglas/index.html');
+  element.target = '_blank';
+});
+credits_links.forEach(element => {
+  element.href = evaluateLiveValue('http://everlasting.lat/colaboradores', '../../colaboradores/index.html');
+  element.target = '_blank';
+});
+
 
 // Functions
 function evaluateLiveValue(val1, val2){return rWeLive ? val1 : val2;}
